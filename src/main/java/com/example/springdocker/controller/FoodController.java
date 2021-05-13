b2af925cf4729eba4d13a2d440a18014003dfd9e
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2021-05-14 01:56:47 +0200

    Change some URL mappings in FoodController

--- src/main/java/com/example/springdocker/controller/FoodController.java
+++ src/main/java/com/example/springdocker/controller/FoodController.java
@@ -3,30 +3,28 @@ package com.example.springdocker.controller;
 import com.example.springdocker.model.Food;
 import com.example.springdocker.service.FoodService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/foods")
 public class FoodController {
     private final FoodService service;
 
-    @GetMapping("/foods")
+    @GetMapping
     public List<Food> getFoods() {
         return service.getFoods();
     }
 
-    @PostMapping("/foods")
+    @PostMapping("/save")
     public void saveNewFood(@RequestBody Food food) {
         service.saveNewFood(food);
     }
 
-    @GetMapping("/foods/cookable")
-    public List<String> getCookableoods() {
+    @GetMapping("/cookable")
+    public List<String> getCookableFoods() {
         return service.getCookableFoods();
     }
 }
